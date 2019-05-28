f2abe83a8348caf395e03621b45213281fb1a613
unknown <exumcodes@gmail.com>
2019-05-27 23:25:34 -0600

    server and html changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const twilioClient = require('./twilioClient');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,6 +8,11 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get('/sendMessage', function(req, res) {
+  twilioClient.sendSms('+17204955546', 'Test Message');
+  res.send('Message Sent')
+})
 
 // Send every request to the React app
 // Define any API routes before this runs
